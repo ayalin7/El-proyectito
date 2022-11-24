9bb7e7e2f7534c4a1b154506ed0918af0e5d5092
martin sepulveda <martinsep1000@gmail.com>
2022-11-24 16:53:16 -0300

    mejora del codigo

--- graficos/grafico-modelo-aexpb.py
+++ graficos/grafico-modelo-aexpb.py
@@ -1,4 +1,4 @@
-from numpy import genfromtxt, mean, std, array, arange, linspace, diagonal, exp
+from numpy import genfromtxt, mean, std, array, arange, linspace, diagonal, log, sqrt
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 plt.style.use('classic')
@@ -11,26 +11,40 @@ err_ang = array([std(data[:,0]),std(data[:,1]),std(data[:,2]),std(data[:,3]),std
 long = arange(10,110,10)
 err_long = [0.05]*10
 
+#---subplots----------------------------------------------------------
+fig, (ax1, ax2) = plt.subplots(1, 2, constrained_layout=True, figsize=(14,7))
+
 #---grafico-----------------------------------------------------------
-plt.errorbar(long, ang, xerr=err_long,  yerr=err_ang, marker='o', color='royalblue', label='datos experimentales')
-plt.grid(True)
-plt.hlines(360, 0, 120, color='red', label='un giro completo', linestyle='--')
-plt.hlines(720, 0, 120, color='darkred', label='dos giros completos', linestyle='--')
-plt.xlabel('Longitud $[cm]$')
-plt.ylabel('Ángulo de giro $[deg]$')
+ax1.errorbar(long, ang, xerr=err_long,  yerr=err_ang, marker='o', color='royalblue', label='datos experimentales')
+ax1.grid(True)
+ax1.hlines(360, 0, 120, color='red', label='un giro completo', linestyle='--')
+ax1.hlines(720, 0, 120, color='darkred', label='dos giros completos', linestyle='--')
+ax1.set_xlabel('Longitud $[cm]$')
+ax1.set_ylabel('Ángulo de giro $[deg]$')
 
 #---modelo-------------------------------------------------------------
-def f(x, a, b):
-	modelo = a + b*exp(x)
-	return modelo
+def f(x, a, b, c, d):
+	return a + b*log(x*c + d)
 
-#---ajuste-de-curva----------------------------------------------------
+#---grafico-modelo-----------------------------------------------------
 pcurv, pcov = curve_fit(f, long, ang, sigma=err_ang, absolute_sigma=True)
 
+sigma_a, sigma_b, sigma_c, sigma_d = sqrt(diagonal(pcov))
+
 longg = linspace(min(long), max(long), len(long)*20)
-plt.plot(longg, f(longg, pcurv[0], pcurv[1]), color='g', label='modelo $a$', linewidth=2)
+ax1.plot(longg, f(longg, pcurv[0], pcurv[1], pcurv[2], pcurv[3]), color='g', label='modelo $a + be^{xc+d}$', linewidth=2)
+
+#---grafico-residuos---------------------------------------------------
+res = ang - f(long, pcurv[0], pcurv[1], pcurv[2], pcurv[3])
+ax2.scatter(long, res/err_ang, color='royalblue', s=40)
+ax2.hlines(0, 0, 120, color='g', linewidth=3)
+ax2.set_xlim(0, 120)
+ax2.grid(True)
+ax2.set_xlabel('Ángulo de giro $[deg]$')
+ax2.set_ylabel('modelo $a + be^{xc+d}$')
+ax2.set_title('gráfico de residuos para el modelo $a + be^{xc+d}$')
 
 #---graficar-----------------------------------------------------------
-plt.title('error a = 2e-5, error b = 1e-9', size=11)
-plt.legend(loc='lower right')
+ax1.set_title('error a = 2e-5, error b = 1e-9', size=11)
+ax1.legend(loc='lower right')
 plt.savefig('grafico-modelo-aexpb.pdf')
